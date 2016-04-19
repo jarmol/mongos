5ef37b62d0ecb59e62fc3b1808c0c66d923e7c29
Jarmo Lammi <jjlammi@yahoo.com>
2016-04-20 00:14:52 +0300

    find car equipments

--- findVaruste.js
+++ findVaruste.js
@@ -4,7 +4,7 @@ var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/book';
 
 var findCars = function(db, callback) {
-   var cursor =db.collection('auto').find({ "varuste": /radio/i });
+   var cursor =db.collection('auto').find({ "varuste": /koukku/i });
    cursor.each(function(err, doc) {
       assert.equal(err, null);
       if (doc != null) {
